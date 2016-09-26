9cff77d21d621ef7c08bbf0f3936d38ad27ca2b7
eis <eis@iki.fi>
2016-09-26 15:07:22 +0300

    updated curl script

--- test-with-curl.sh
+++ test-with-curl.sh
@@ -1,1 +1,1 @@
-curl -vv --header "Content-Type: text/xml" --header "Accept: text/xml,*/*" -d @request.xml http://localhost:8080/ws
+curl -vv --header "Content-Type: text/xml" --header "Accept: text/xml,*/*" -d @request.xml http://user:pass@localhost:8080/ws
